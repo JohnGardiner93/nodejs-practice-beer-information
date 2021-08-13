9c0e581f4de97f7b9e8fe0e3d97d1bab0e0700ed
JohnGardiner93 <john.gardiner93@gmail.com>
2021-08-13 06:53:35 -0400

    index.js Refactor - replaceTemplateText & Variable Names
    Refactors fillText methods into one template filling method. Changes
    variable names to be more clear.

--- index.js
+++ index.js
@@ -13,44 +13,36 @@ SVGFILEPATHS = [
 
 ////////////////////////////////////////////
 // Functions
-const fillText = function (temp, data) {
+const replaceTemplateText = function (temp, data) {
   let output = temp.replace(`{%CSSSTYLING%}`, data);
-  return output;
-};
 
-const fillTapCard = function (temp, beer) {
-  let output = temp
-    .replace(`{%ID%}`, beer?.id)
-    .replace(`{%COLOR%}`, beer?.color)
-    .replace(`{%HEADCOLOR%}`, beer?.headColor)
-    .replace(`{%NAME%}`, beer?.name)
-    .replace(`{%STYLE%}`, beer?.style)
-    .replace(`{%ABV%}`, beer?.abv)
-    .replace(`{%IBU%}`, beer?.ibu)
-    .replace(`{%DESCRIPTION%}`, beer?.description)
-    .replace(`{%RELEASEDATE%}`, beer?.releaseDate);
-  return output;
-};
+  // Check if the data is a beer.
+  // TODO - add id back in once non-numerical id system is implemented
+  if (data?.color) {
+    output = output
+      .replace(`{%ID%}`, data?.id)
+      .replace(`{%COLOR%}`, data?.color)
+      .replace(`{%HEADCOLOR%}`, data?.headColor)
+      .replace(`{%NAME%}`, data?.name)
+      .replace(`{%STYLE%}`, data?.style)
+      .replace(`{%ABV%}`, data?.abv)
+      .replace(`{%IBU%}`, data?.ibu)
+      .replace(`{%DESCRIPTION%}`, data?.description)
+      .replace(`{%RELEASEDATE%}`, data?.releaseDate)
+      .replace(`{%IMAGEPATH%}`, data?.imagePath)
+      .replace(`{%INGREDIENT_MALT%}`, data?.ingredients?.malt.join(`, `))
+      .replace(`{%INGREDIENT_HOPS%}`, data?.ingredients?.hops.join(`, `))
+      .replace(`{%INGREDIENT_YEAST%}`, data?.ingredients?.yeast.join(`, `))
+      .replace(`{%INGREDIENT_EXTRA%}`, data?.ingredients?.extra.join(`, `))
+      .replace(`{%INGREDIENT_FRUIT%}`, data?.ingredients?.fruit.join(`, `));
+  }
 
-const fillProductPage = function (temp, beer) {
-  let output = temp
-    .replace(`{%IMAGEPATH%}`, beer?.imagePath)
-    .replace(`{%NAME%}`, beer?.name)
-    .replace(`{%STYLE%}`, beer?.style)
-    .replace(`{%ABV%}`, beer?.abv)
-    .replace(`{%IBU%}`, beer?.ibu)
-    .replace(`{%DESCRIPTION%}`, beer?.description)
-    .replace(`{%INGREDIENT_MALT%}`, beer?.ingredients?.malt.join(`, `))
-    .replace(`{%INGREDIENT_HOPS%}`, beer?.ingredients?.hops.join(`, `))
-    .replace(`{%INGREDIENT_YEAST%}`, beer?.ingredients?.yeast.join(`, `))
-    .replace(`{%INGREDIENT_EXTRA%}`, beer?.ingredients?.extra.join(`, `))
-    .replace(`{%INGREDIENT_FRUIT%}`, beer?.ingredients?.fruit.join(`, `));
   return output;
 };
 
 const constructTapCard = function (temp, svgs, beer) {
   // Construct SVG's
-  let output = fillTapCard(temp, beer);
+  let output = replaceTemplateText(temp, beer);
   let svgElements = svgs.map((svg) => constructSVG(svg, beer)).join("");
   output = output.replace(`{%PINTSVG%}`, svgElements);
   return output;
@@ -65,23 +57,34 @@ const constructSVG = function (svg, beer) {
 
 ////////////////////////////////////////////
 // File Reads
-const home = fs.readFileSync(`${__dirname}/templates/overview.html`, `utf-8`);
-const tapCard = fs.readFileSync(
-  `${__dirname}/templates/tap-card.html`,
+// Stylesheets
+const stylesheetMain = fs.readFileSync(
+  `${__dirname}/styles/style.css`,
   `utf-8`
 );
-const productDetails = fs.readFileSync(
-  `${__dirname}/templates/product-details.html`,
-  `utf-8`
-);
-const style = fs.readFileSync(`${__dirname}/styles/style.css`, `utf-8`);
-const iconStylesheet = fs.readFileSync(
+const stylesheetIcons = fs.readFileSync(
   `${__dirname}/styles/icons.css`,
   `utf-8`
 );
+
+// HTML Templates
+const templateHome = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  `utf-8`
+);
+const templateTapCard = fs.readFileSync(
+  `${__dirname}/templates/tap-card.html`,
+  `utf-8`
+);
+const templateProductDetails = fs.readFileSync(
+  `${__dirname}/templates/product-details.html`,
+  `utf-8`
+);
+const templatesSVG = SVGFILEPATHS.map((path) => fs.readFileSync(path, `utf-8`));
+
+// Data
 const beerData = fs.readFileSync(`${__dirname}/data/beer-data.json`, `utf-8`);
 const beers = JSON.parse(beerData);
-const svgs = SVGFILEPATHS.map((path) => fs.readFileSync(path, `utf-8`));
 
 ////////////////////////////////////////////
 // Server Logic
@@ -90,39 +93,51 @@ const server = http.createServer((req, res) => {
 
   // Serve home page
   if (pathname === `/` || pathname === "/home") {
-    res.writeHead(200, `text/html`);
-    const newHome = fillText(home, style);
-    // const beerCards = constructTapCard(tapCard, svgs, beers[0]);
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
     const beerCards = beers
-      .map((beer) => constructTapCard(tapCard, svgs, beer))
+      .map((beer) => constructTapCard(templateTapCard, templatesSVG, beer))
       .join("");
-    const newHome2 = newHome.replace(`{%TAPCARDS%}`, beerCards);
-    res.end(newHome2);
+    const home = replaceTemplateText(templateHome, stylesheetMain).replace(
+      `{%TAPCARDS%}`,
+      beerCards
+    );
+    res.end(home);
   }
+
   // Detail Pages
   else if (pathname === `/beer`) {
-    res.writeHead(200, `text/html`);
-    const newPage = fillText(productDetails, style);
-
-    const beerDetailPage = fillProductPage(newPage, beers[query.id]);
-
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    const newPage = replaceTemplateText(templateProductDetails, stylesheetMain);
+    const beerDetailPage = replaceTemplateText(newPage, beers[query.id]);
     res.end(beerDetailPage);
   }
+
   // Image
   else if (pathname.includes(`/img`)) {
-    // console.log(`this is the image request`);
-    // console.log(pathname);
     const img = fs.readFileSync(`.${pathname}`);
     res.writeHead(200, { "Content-Type": "image/jpg" });
     res.end(img, "binary");
   }
+
   // Icon Stylesheet
   else if (pathname.includes(`/styles`)) {
-    res.writeHead(200, `text/html`);
-    res.end(iconStylesheet);
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(stylesheetIcons);
   }
 
   // Unknown Page
+  else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end(`<h1>Page not found!</h1>`);
+  }
 });
 
 server.listen(8000, `127.0.0.1`, () => {
