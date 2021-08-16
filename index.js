6ce8cf5e5ccaf4edb05729205e83986a19b74549
JohnGardiner93 <john.gardiner93@gmail.com>
2021-08-16 06:18:32 -0400

    Page Construction Refactor
    Refactors functions that build pages to be more logically organized and
    DRY. Adds documentation for element and page construction methods.

--- index.js
+++ index.js
@@ -13,45 +13,79 @@ SVGFILEPATHS = [
 
 ////////////////////////////////////////////
 // Functions
-const replaceTemplateText = function (temp, data) {
-  let output = temp.replace(`{%CSSSTYLING%}`, data);
-
-  // Check if the data is a beer.
-  // TODO - add id back in once non-numerical id system is implemented
-  if (data?.color) {
-    output = output
-      .replace(`{%ID%}`, data?.id)
-      .replace(`{%COLOR%}`, data?.color)
-      .replace(`{%HEADCOLOR%}`, data?.headColor)
-      .replace(`{%NAME%}`, data?.name)
-      .replace(`{%STYLE%}`, data?.style)
-      .replace(`{%ABV%}`, data?.abv)
-      .replace(`{%IBU%}`, data?.ibu)
-      .replace(`{%DESCRIPTION%}`, data?.description)
-      .replace(`{%RELEASEDATE%}`, data?.releaseDate)
-      .replace(`{%IMAGEPATH%}`, data?.imagePath)
-      .replace(`{%INGREDIENT_MALT%}`, data?.ingredients?.malt.join(`, `))
-      .replace(`{%INGREDIENT_HOPS%}`, data?.ingredients?.hops.join(`, `))
-      .replace(`{%INGREDIENT_YEAST%}`, data?.ingredients?.yeast.join(`, `))
-      .replace(`{%INGREDIENT_EXTRA%}`, data?.ingredients?.extra.join(`, `))
-      .replace(`{%INGREDIENT_FRUIT%}`, data?.ingredients?.fruit.join(`, `));
-  }
-
-  return output;
-};
-
-const constructTapCard = function (temp, svgs, beer) {
-  // Construct SVG's
-  let output = replaceTemplateText(temp, beer);
-  let svgElements = svgs.map((svg) => constructSVG(svg, beer)).join("");
-  output = output.replace(`{%PINTSVG%}`, svgElements);
-  return output;
-};
-
-const constructSVG = function (svg, beer) {
-  const output = svg
+/**
+ * Fills a template in with data about the provided beer, if the data exists. Does not supply award data to the template.
+ * @param {String} template - HTML template to be filled in.
+ * @param {Object} beer - Beer object with information that will be placed into template.
+ * @returns {String} - Filled template.
+ */
+const fillInBeerData = function (template, beer) {
+  let output = template
+    .replace(`{%ID%}`, beer?.id)
+    .replace(`{%NAME%}`, beer?.name)
+    .replace(`{%STYLE%}`, beer?.style)
+    .replace(`{%ABV%}`, beer?.abv)
+    .replace(`{%IBU%}`, beer?.ibu)
     .replace(`{%COLOR%}`, beer?.color)
-    .replace(`{%HEADCOLOR%}`, beer?.headColor);
+    .replace(`{%HEADCOLOR%}`, beer?.headColor)
+    .replace(`{%DESCRIPTION%}`, beer?.description)
+    .replace(`{%IMAGEPATH%}`, beer?.imagePath)
+    .replace(`{%INGREDIENT_MALT%}`, beer?.ingredients?.malt.join(`, `))
+    .replace(`{%INGREDIENT_HOPS%}`, beer?.ingredients?.hops.join(`, `))
+    .replace(`{%INGREDIENT_YEAST%}`, beer?.ingredients?.yeast.join(`, `))
+    .replace(`{%INGREDIENT_EXTRA%}`, beer?.ingredients?.extra.join(`, `))
+    .replace(`{%INGREDIENT_FRUIT%}`, beer?.ingredients?.fruit.join(`, `));
+  return output;
+};
+
+// Sub-Element Construction Functions
+/**
+ * Constructs a tap card that can be displayed on the overview page using the provided template, svg templates, and beer data.
+ * @param {String} template - Tap card HTML template.
+ * @param {String} svgs - SVG templates used to construct pint image using beer data.
+ * @param {Object} beer - Beer object containing desired data.
+ * @returns {String} - Filled template.
+ */
+const constructTapCard = function (template, svgs, beer) {
+  // Construct SVG's
+  let output = template.replace(`{%PINTSVG%}`, svgs);
+  output = fillInBeerData(output, beer);
+  return output;
+};
+
+  return output;
+};
+
+// Page-Building Functions
+/**
+ * Builds overview page that contains tap cards representing each beer as provided in an array of beer objects.
+ * @param {String} tapCardTemplate - HTML template for tap cards.
+ * @param {String} SVGTemplate - HTML template for pint image svgs.
+ * @param {Array[Object]} beers - Array of beer objects to be displayed.
+ * @returns {String} - Filled overview template with beer information displayed.
+ */
+const buildOverviewPage = function (tapCardTemplate, SVGTemplate, beers) {
+  const beerCards = beers
+    .map((beer) => constructTapCard(tapCardTemplate, SVGTemplate, beer))
+    .join("");
+  const overview = templateHome.replace(`{%TAPCARDS%}`, beerCards);
+  return overview;
+};
+
+/**
+ * Builds Product detail page that displays information about the requested beer.
+ * @param {String} productDetailTemplate - HTML template for product detail page.
+ * @param {String} awardTextTemplate - HTML template for award text.
+ * @param {Object} beer - Beer object to be displayed.
+ * @returns
+ */
+const buildProductDetailPage = function (
+  productDetailTemplate,
+  awardTextTemplate,
+  beer
+) {
+  let output = "";
+  output = fillInBeerData(output, beer);
   return output;
 };
 
@@ -80,7 +114,9 @@ const templateProductDetails = fs.readFileSync(
   `${__dirname}/templates/product-details.html`,
   `utf-8`
 );
-const templatesSVG = SVGFILEPATHS.map((path) => fs.readFileSync(path, `utf-8`));
+const templateSVGs = SVGFILEPATHS.map((path) =>
+  fs.readFileSync(path, `utf-8`)
+).join("");
 
 // Data
 const beerData = fs.readFileSync(`${__dirname}/data/beer-data.json`, `utf-8`);
@@ -96,14 +132,8 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    const beerCards = beers
-      .map((beer) => constructTapCard(templateTapCard, templatesSVG, beer))
-      .join("");
-    const home = replaceTemplateText(templateHome, stylesheetMain).replace(
-      `{%TAPCARDS%}`,
-      beerCards
-    );
-    res.end(home);
+    const overview = buildOverviewPage(templateTapCard, templateSVGs, beers);
+    res.end(overview);
   }
 
   // Detail Pages
@@ -111,8 +141,11 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
-    const newPage = replaceTemplateText(templateProductDetails, stylesheetMain);
-    const beerDetailPage = replaceTemplateText(newPage, beers[query.id]);
+    const beerDetailPage = buildProductDetailPage(
+      templateProductDetails,
+      templateAwardText,
+      beers[query.id]
+    );
     res.end(beerDetailPage);
   }
 
